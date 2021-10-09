a38950c08c362e969333bb20585448da7d14a9bf
Joško Todić <josko.todic@avl.com>
2021-10-10 00:52:59 +0200

    port 3

--- src/hello-app.py
+++ src/hello-app.py
@@ -12,17 +12,17 @@ put_table([
     ['Owner', "Joško T."]
     ])
 
-#if __name__ == '__main__':
-#    import argparse
-#    from pywebio.platform.tornado_http import start_server as start_http_server
-#    from pywebio import start_server as start_ws_server
-#
-#    parser = argparse.ArgumentParser()
-#    parser.add_argument("-p", "--port", type=int, default=8080)
-#    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
-#    args = parser.parse_args()
-#
-#    if args.http:
-#        start_http_server(main, port=args.port)
-#    else:
-#        start_ws_server(main, port=args.port, websocket_ping_interval=30)
+if __name__ == '__main__':
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
+
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
