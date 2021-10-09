b6f1df93fc52c3e29e6fd0fff489366994f4abd3
Joško Todić <josko.todic@avl.com>
2021-10-10 00:21:38 +0200

    port setup

--- src/hello-app.py
+++ src/hello-app.py
@@ -1,7 +1,6 @@
 from pywebio.input import *
 from pywebio.output import *
 
-
 input("What's your name?")
 select("Select food", ['Orange', 'Apple', 'Banana'])
 checkbox("Are your okay?", options=["I'm okay."])
@@ -12,3 +11,18 @@ put_table([
     ['Hello-app version', '1.0.0'],
     ['Owner', "Joško T."]
     ])
+
+if __name__ == '__main__':
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
+
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
