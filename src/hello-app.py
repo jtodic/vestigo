9167df826d78f8e8e53984a75560f88e3804686a
Joško Todić <josko.todic@avl.com>
2021-10-10 01:30:19 +0200

    port 4

--- src/hello-app.py
+++ src/hello-app.py
@@ -1,16 +1,17 @@
 from pywebio.input import *
 from pywebio.output import *
 
-input("What's your name?")
-select("Select food", ['Orange', 'Apple', 'Banana'])
-checkbox("Are your okay?", options=["I'm okay."])
-radio("What do you like to do?", options=['Eat', 'Sleep', 'Study'])
-put_image(open('vestigo-logo.png', 'rb').read())
+def main():
+    input("What's your name?")
+    select("Select food", ['Orange', 'Apple', 'Banana'])
+    checkbox("Are your okay?", options=["I'm okay."])
+    radio("What do you like to do?", options=['Eat', 'Sleep', 'Study'])
+    put_image(open('vestigo-logo.png', 'rb').read())
 
-put_table([
-    ['Hello-app version', '1.0.0'],
-    ['Owner', "Joško T."]
-    ])
+    put_table([
+        ['Hello-app version', '1.0.0'],
+        ['Owner', "Joško T."]
+        ])
 
 if __name__ == '__main__':
     import argparse
