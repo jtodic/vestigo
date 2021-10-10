12ad156144449fa5c4d001ec56c90f922f0436d9
Joško Todić <josko.todic@avl.com>
2021-10-10 15:24:23 +0200

    version 2

--- src/hello-app.py
+++ src/hello-app.py
@@ -9,7 +9,7 @@ def main():
     put_image(open('vestigo-logo.png', 'rb').read())
 
     put_table([
-        ['Hello-app version', '1.0.0'],
+        ['Hello-app version', '2.0.0'],
         ['Owner', "Joško T."]
         ])
 
